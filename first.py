65c918e2786bcfb90c5e68a103fe3fc34d7185bd
ilokeshrao <ly0367578@gmail.com>
2025-12-03 00:08:10 +0530

    your message

---
+++ first.py
@@ -0,0 +1,9 @@
+
+
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("my namee is lokesh yadav")
+
+engine.runAndWait()
---
+++ variable.py
@@ -0,0 +1,38 @@
+
+# Q 1
+
+# w.a.p tto to input the average of two number 
+
+# a = int(input("Enter a number 1 : "))
+# b = int(input("Enter a number 2 : "))
+
+# print("Average the number is : ", (a+b)/2)
+
+
+
+
+
+# Q 2
+
+# //comparision operator 
+
+# a = 40
+# b = 70
+
+# a = int(input("Enter a number 1 : "))
+# b = int(input("Enter a number 2 : "))
+
+# print("Comaprision the operator is  : ", a>b)
+
+
+
+
+# Q 3
+
+# W.A.P to calculate the square of a number entered the user 
+
+
+a = int(input("Enter a number 1 : "))
+
+
+print("calculate the square of the number is :", a*a)
