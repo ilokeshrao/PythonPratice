6d5bf63c09cf87ac9d048dae9dbf26ceb53a83f2
ilokeshrao <ly0367578@gmail.com>
2025-12-05 23:49:02 +0530

    Initial upload

---
+++ escape.py
@@ -0,0 +1,17 @@
+
+
+# a = "Lokesh is a god boy \n and playing a \"cricket\" "
+
+# print(a)
+
+
+
+# // Write a program to enter goofafter noon //
+
+# a = input("Enter your name : ")
+
+# print(f"my name is ",a)
+
+letter  = ''' Lokesh \n  23/11/2003'''
+
+print(letter)
