8c786703179be854401631b746fbfb5f59e4d11b
ilokeshrao <ly0367578@gmail.com>
2025-12-07 09:58:06 +0530

    Added python files

---
+++ list.py
@@ -0,0 +1,28 @@
+
+
+
+# list all method 
+
+# list = ["lokesh","orange","applee","depuuu"]
+
+# print(list)
+
+# print(list.append(4))
+
+# print(list.insert(2,100))
+# 
+
+# print(list.pop(3))
+# print(list)
+
+
+
+
+
+list = ["lokesh","orange","applee","depuuu"]
+
+
+
+print(list[1])
+list[1]=  "Goluu"
+print(list)
---
+++ tuples.py
@@ -0,0 +1,48 @@
+
+
+
+#  tuples are immutable 
+
+
+# a = ("lokesh","deepu",22,22.70,22,40)
+
+# # print(a.count(22))
+# print(a.index(40))
+
+
+
+# Q1 =  W.A.P to store seven fruits entered list user 
+
+# fruits = input("Enter the Seven  fruits name : ")
+
+# print("Fruiits Name is : ",fruits)
+
+
+# Marks  = input("Enter the six marks : ")
+
+
+# mark = Marks.split()
+
+# mark =  list(map(int,mark))
+
+# mark.sort()
+
+
+# print(mark)
+
+
+fruits  =  []
+
+f1 = input("enter the fruits name :")
+fruits.append(f1)
+
+f2= input("enter the fruits name :")
+fruits.append(f2)
+f3 = input("enter the fruits name :")
+fruits.append(f3)
+f4= input("enter the fruits name :")
+fruits.append(f4)
+f5 = input("enter the fruits name :")
+fruits.append(f5)
+
+print(fruits)
