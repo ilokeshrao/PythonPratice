c12b42a446edc5dd9ac180adb4ab7fdc0c5b79a8
ilokeshrao <ly0367578@gmail.com>
2025-12-07 20:40:26 +0530

    Add/Update Python course files

---
+++ dictset.py
@@ -0,0 +1,33 @@
+
+Marks =  {
+    "name" :  "lokesh",
+    "age" : 21,
+    "address": "rewari",
+    "add" :"eeee"
+}
+
+
+# print(Marks.items())
+# print(Marks.keys())
+# print(Marks.values())
+# Marks.update({"name": "lokesh"})  
+# print(Marks)
+
+# print(Marks.get("name"))  # None
+# print(Marks["name"])      #Error 
+# print(Marks.pop("add"))
+# print(Marks.popitem())
+# print(Marks.setdefault("gurgoan"))
+
+# copymarks = Marks.copy()
+
+# Marks["age"]=  25
+
+# print(Marks)
+# print(copymarks)
+
+print(Marks.update({"name": "depuu"}))
+print(Marks)
+
+
+
--- tuples.py
+++ tuples.py
@@ -30,19 +30,55 @@
 
 # print(mark)
 
-
-fruits  =  []
+fruits =  []
 
 f1 = input("enter the fruits name :")
 fruits.append(f1)
 
-f2= input("enter the fruits name :")
+f2 = input("enter the fruits name :")
 fruits.append(f2)
+
 f3 = input("enter the fruits name :")
 fruits.append(f3)
-f4= input("enter the fruits name :")
+
+f4 = input("enter the fruits name :")
 fruits.append(f4)
+
 f5 = input("enter the fruits name :")
 fruits.append(f5)
 
 print(fruits)
+
+# marks=  []
+
+# for i in range(7):
+#     mark=  int(input("enter the marks : "))
+#     marks.append(mark)
+
+# marks.sort()
+# print("marks list :",marks)
+
+
+
+# marks = [23, 25, 2, 19, 17]
+
+# for i in range(5):
+#     marks.sort()
+#     print("Mark:", marks[i])
+
+
+# a = (23,44 ,"lokesh",67.8)
+
+# print(type(a))
+
+# s = (23,4,2,1,5)
+
+# print(sum(s))
+
+# a=  [9,0,4,0,4,0,5,5,0,0]
+
+# # print(a.count(0))
+
+# print(a[2])
+# a[2] = 5
+# print(a)
